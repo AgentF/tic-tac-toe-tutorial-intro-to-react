dc7ea39e1facb78b17176a99ffac659072770f62
AgentF <freddyfx231@gmail.com>
2019-08-13 15:37:42 -0400

    Updates

---
+++ precache-manifest.1811d7fa8ce721dc9177d2d1e55a06bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2ae550d48d757a61b9829dcc7c147f",
+    "url": "/tic-tac-toe-tutorial-intro-to-react/index.html"
+  },
+  {
+    "revision": "a1be8a580b291c1a2c2e",
+    "url": "/tic-tac-toe-tutorial-intro-to-react/static/css/main.4de68f0f.chunk.css"
+  },
+  {
+    "revision": "4e3d7047f014ec8e24ae",
+    "url": "/tic-tac-toe-tutorial-intro-to-react/static/js/2.34a3dab9.chunk.js"
+  },
+  {
+    "revision": "a1be8a580b291c1a2c2e",
+    "url": "/tic-tac-toe-tutorial-intro-to-react/static/js/main.3b7b4190.chunk.js"
+  },
+  {
+    "revision": "9f3b4118610ba905a21b",
+    "url": "/tic-tac-toe-tutorial-intro-to-react/static/js/runtime~main.80640557.js"
+  }
+]);
